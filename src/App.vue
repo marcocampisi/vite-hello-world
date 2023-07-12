543a40375a6f45e873cd3cfb08eece288386b12d
marcocampisi <marco.campisi@outlook.it>
2023-07-12 14:40:34 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return
+  }
+}
+</script>
+
+<template>
+  <body>
+    <div class="helloWorld">
+      Hello World!
+    </div>
+  </body>
+</template>
+
+<style scoped>
+.helloWorld {
+  font-weight: bold;
+  font-size: 3rem;
+  text-align: center;
+}
+</style>
